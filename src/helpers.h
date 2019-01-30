65761dec8f2a31be952376a031403a00b088dbd4
rons-git <ron@cancunron.com>
2019-01-29 19:57:28 -0500

    update from windows

--- src/helpers.h
+++ src/helpers.h
@@ -1,3 +1,4 @@
+
 #ifndef HELPERS_H
 #define HELPERS_H
 
@@ -11,15 +12,16 @@ using std::string;
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
-    return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.rfind("}]");
+	if (found_null != string::npos) {
+		return "";
+	}
+	else if (b1 != string::npos && b2 != string::npos) {
+		return s.substr(b1, b2 - b1 + 2);
+	}
+	return "";
 }
 
 //
@@ -28,36 +30,36 @@ string hasData(string s) {
 
 // Evaluate a polynomial.
 double polyeval(const VectorXd &coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); ++i) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
+	double result = 0.0;
+	for (int i = 0; i < coeffs.size(); ++i) {
+		result += coeffs[i] * pow(x, i);
+	}
+	return result;
 }
 
 // Fit a polynomial.
 // Adapted from:
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 VectorXd polyfit(const VectorXd &xvals, const VectorXd &yvals, int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
+	assert(xvals.size() == yvals.size());
+	assert(order >= 1 && order <= xvals.size() - 1);
 
-  Eigen::MatrixXd A(xvals.size(), order + 1);
+	Eigen::MatrixXd A(xvals.size(), order + 1);
 
-  for (int i = 0; i < xvals.size(); ++i) {
-    A(i, 0) = 1.0;
-  }
+	for (int i = 0; i < xvals.size(); ++i) {
+		A(i, 0) = 1.0;
+	}
 
-  for (int j = 0; j < xvals.size(); ++j) {
-    for (int i = 0; i < order; ++i) {
-      A(j, i + 1) = A(j, i) * xvals(j);
-    }
-  }
+	for (int j = 0; j < xvals.size(); ++j) {
+		for (int i = 0; i < order; ++i) {
+			A(j, i + 1) = A(j, i) * xvals(j);
+		}
+	}
 
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
+	auto Q = A.householderQr();
+	auto result = Q.solve(yvals);
 
-  return result;
+	return result;
 }
 
 #endif  // HELPERS_H
