a43a477f5f3c71e9acf80e815eba9d624d84717e
rons-git <ron@cancunron.com>
2019-01-29 20:24:01 -0500

    update from windows

--- src/helpers.h
+++ src/helpers.h
@@ -40,25 +40,24 @@ double polyeval(const VectorXd &coeffs, double x) {
 // Fit a polynomial.
 // Adapted from:
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-VectorXd polyfit(const VectorXd &xvals, const VectorXd &yvals, int order) {
+VectorXd polyfit(VectorXd xvals, VectorXd yvals, int order) {
 	assert(xvals.size() == yvals.size());
 	assert(order >= 1 && order <= xvals.size() - 1);
 
 	Eigen::MatrixXd A(xvals.size(), order + 1);
 
-	for (int i = 0; i < xvals.size(); ++i) {
+	for (int i = 0; i < xvals.size(); i++) {
 		A(i, 0) = 1.0;
 	}
 
-	for (int j = 0; j < xvals.size(); ++j) {
-		for (int i = 0; i < order; ++i) {
+	for (int j = 0; j < xvals.size(); j++) {
+		for (int i = 0; i < order; i++) {
 			A(j, i + 1) = A(j, i) * xvals(j);
 		}
 	}
 
 	auto Q = A.householderQr();
 	auto result = Q.solve(yvals);
-
 	return result;
 }
 
